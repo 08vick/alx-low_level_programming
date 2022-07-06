999e42d146d1c527a0b84147a75d6c95966aff8b
root <vickodhis.vn@gmail.com>
2022-07-06 15:01:11 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
-#include <unistd.h>
 #include "main.h"
-
 /**
-*main - check description
-*Description: It prints the word putchar, followed by a new line.
-Return: 0.
+*main - Entry point
+*
+*Return: always (0) success
 */
 int main(void)
 {
-char word[9] = "_putchar";
-int i;
-
-for (i = 0; i < 9; i++)
-_putchar(word[i]);
-_putchar('\n'
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
 return (0);
 }
