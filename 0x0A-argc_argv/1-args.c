0659342091e2efebfb882b4282742abacd922607
root <vickodhis.vn@gmail.com>
2022-07-24 10:50:44 -0700

    silence

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,15 @@
+#include "main.h"
 #include <stdio.h>
+
 /**
-*main - display num of arg
-*@argc: int
-*@argv: string
-*Return: int
+*main - A problem that prints the number of arguments you passed into it
+*@argc: An argument counter
+*@argv: An argument values
+*Return: Always 0 (success)
 */
-int main(int argc, char *argv[] __attribute__((unused)))
-
-
+int main(int argc, char *argv[])
+{
+if (argv[0])
 printf("%d\n", argc - 1);
-
 return (0);
 }
