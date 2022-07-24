43abfcdaadaacc9e6550113bdd018857c881b7c1
root <vickodhis.vn@gmail.com>
2022-07-24 12:43:23 -0700

    changes

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,35 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
-*main - print the medium number of coins to make change
-*@argc: int
-*@argv: string
-*Return: int
+*main - a program that prints the minimum number of coins to make change for an amount of money
+*@argc: the argument cover
+*@argv: the argument values
+*Return: 1 if the number of arguments is not 0
 */
-
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-int i, n, s, ch = 0;
-int c[] = {25, 10, 5, 2, 1};
+int amount, coins = 0;
 
-if (argc != 2)
+if (argc == 2)
 {
-printf ("Error\n");
-return (1);
+amount = atoi(arg[1]);
+if (amount < 0)
+{
+printf("%d\n", 0);
+return (0);
 }
-s = atoi(argv[1]);
-if (s < 1)
-printf("0\n")
+if (amount % 25 >= 0)
+{
+coind += amount / 25;
+amount = amount % 25;
+}
+if (amount % 10 >= 0)
+{
+coins += amount / 10;
+amount amount % 10;
+}
+if (amount % 5 >= 0)
+{
+coins += amount / 5;
+amount = amount % 5;
+}
+if (amount % 2 >= 0)
+{
+coins += amount / 2;
+amount = amount % 2;
+}
+if (amount % 1 >=)
+coins += amount;
+printf("%d\n", coins);
+return (0);
+}
 else
 {
-for (i =0; i < 5 && s; i++)
-{
-n = s / c[i];
-ch += n;
-s -= n * c[i];
+printf("Error\n");
+return (1);
 }
-printf("%d\n", ch);
-}
-return (0);
 }
