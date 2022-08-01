8d3ec7b8038f6941508f2b855e620aecf1c56f5d
root <vickodhis.vn@gmail.com>
2022-08-01 03:01:44 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,22 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
+
 /**
-*struct dog - a new type
-*@name : string
-*@age : float
-*@owner : string
-*description: a new type that contains dog name, age, and owner
+*struct dog - structure for a basic dog
+*@name: Dog's name
+*@age: Dog's age
+*@owner: Dog's owner
+*Descriptions: data structure for dog
 */
-typede struct dog
+struct dog
 {
 char *name;
 float age;
 char *owner;
-} dog_t;
+};
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct_dog *d);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif
