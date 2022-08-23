889d20f584b3164dcdcc535f910b8288d3bb42ff
root <vickodhis.vn@gmail.com>
2022-08-23 01:06:52 -0700

    main

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,14 +1,6 @@
-#ifndef FILE_IO
-#define FILE_IO
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-int _putchar(char c);
+#ifndef _MAIN_H_
+#define _MAIN_H_
 ssize_t read_textfile(const char *filename, size_t letters);
-int creat_file(const *filename, char *text_content);
+int create_file(const cahr *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
-#endif
+#endif /* _MAIN_H_ */
