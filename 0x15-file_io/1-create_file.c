207b00b1cd2f1c6656471aa4807b45e45dd0a8a9
root <vickodhis.vn@gmail.com>
2022-08-23 10:50:43 -0700

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,10 +3,10 @@
 /**
 *create_file - creates a file
 *@filename: the name of the file to create
-*@text_content: a NULL terminated string to write to the file
+*@text_contentent: a NULL terminated string to write to the file
 *Return: 1 on success, -1 on failure
 */
-int create_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_contentent)
 {
 int file, fwrite, i;
 
@@ -16,11 +16,11 @@ file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 if (file == -1)
 return (-1);
 
-if (text_content != NULL)
+if (text_contentent != NULL)
 {
-for (i = 0; text_cont[i]; i++)
+for (i = 0; text_content[i]; i++)
 ;
-fwrite = write(file, text_content, i);
+fwrite = write(file, text_contentent, i);
 if (fwrite == -1)
 return (-1);
 }
